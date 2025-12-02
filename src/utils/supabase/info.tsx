2ef7c52586c2748908a79354dfc9f996e91f198b
beingjohannes <jhesford@proton.me>
2025-12-02 11:23:17 +0100

    Replaced with correct server

--- src/utils/supabase/info.tsx
+++ src/utils/supabase/info.tsx
@@ -8,7 +8,7 @@ export const publicAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsaGRodGdwd3diamtrc3JuZWhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2NjI3MjAsImV4cCI6MjA3OTIzODcyMH0.nruSxT1y14nCno3LWB0Np7zV2nUfXoQH1Uyb7HiMtlU";
 
 // The name of the Edge Function we know is healthy
-export const serverSlug = "make-server-6ff8009f";
+export const serverSlug = "server";
 
 // Base URL that the rest of the app should use
 export const serverUrl = `https://${projectId}.supabase.co/functions/v1/${serverSlug}`;
